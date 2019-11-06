1748344743a446d4a10c0f57c7fd29375e37849f
WinPooh32 <winpooh32@yandex.ru>
2019-11-06 16:49:27 +1000

    Fix client api generator script

--- api/client.generate.sh
+++ api/client.generate.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 version=`cat version`
+output="../frontend/assets/todo"
 
-#https://github.com/OpenAPITools/openapi-generator
-openapi-generator generate -i $version/openapi.oas3.yaml -g typescript-axios -o ../frontend/assets/api
+# https://github.com/OpenAPITools/openapi-generator
+openapi-generator generate -i $version/openapi.oas3.yaml -g javascript -o $output
+
+# replace index file path in package.json
+sed -i 's/\"dist\/index.js\"/\"src\/index.js\"/g' $output/package.json
+
+# install package build dependencies
+cd $output
+npm install
