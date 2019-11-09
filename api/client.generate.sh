d79cd689ce2154162cf9cb4e1a687c1c8795bb91
WinPooh32 <winpooh32@yandex.ru>
2019-11-09 12:59:26 +1000

    Add usePromises=true

--- api/client.generate.sh
+++ api/client.generate.sh
@@ -4,7 +4,10 @@ version=`cat version`
 output="../frontend/assets/todo"
 
 # https://github.com/OpenAPITools/openapi-generator
-openapi-generator generate -i $version/openapi.oas3.yaml -g javascript -o $output
+openapi-generator generate \
+ --additional-properties=usePromises=true \
+ -i $version/openapi.oas3.yaml \
+ -g javascript -o $output
 
 # replace index file path in package.json
 sed -i 's/\"dist\/index.js\"/\"src\/index.js\"/g' $output/package.json
